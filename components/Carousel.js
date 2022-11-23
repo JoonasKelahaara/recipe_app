b207ac8afc176e8bbe0893bcf8c1016de02fe6db
henni saaranen <n0sahe00@students.oamk.fi>
2022-11-23 14:40:30 +0200

    karusellin testailua

---
+++ components/Carousel.js
@@ -0,0 +1,63 @@
+/* import React from "react";
+import {defaultStyle} from '../styles/styles.js';
+import Carousel from 'react-native-snap-carousel';
+
+export class MyCarousel extends Component {
+
+    _renderItem = ({item, index}) => {
+        return (
+            <View style={styles.slide}>
+                <Text style={styles.title}>{ item.title }</Text>
+            </View>
+        );
+    }
+
+    render () {
+        return (
+            <Carousel
+              ref={(c) => { this._carousel = c; }}
+              data={this.state.entries}
+              renderItem={this._renderItem}
+              sliderWidth={sliderWidth}
+              itemWidth={itemWidth}
+            />
+        );
+    }
+}
+ */
+/* import React from "react";
+import { Dimensions, Text, Image } from 'react-native';
+import {defaultStyle} from '../styles/styles.js';
+import Carousel from 'react-native-reanimated-carousel';
+
+function Index() {
+    const width = Dimensions.get('window').width;
+    return (
+        <View style={{ flex: 1 }}>
+            <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={[...new Array(6).keys()]}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ index }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
+
+export default Index;  */
