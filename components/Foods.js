dde83e6af66a025bc36dc17e1add4d5b5f76b927
henni saaranen <n0sahe00@students.oamk.fi>
2022-12-03 18:33:52 +0200

    Ruoka-sivun jatkamista

--- components/Foods.js
+++ components/Foods.js
@@ -1,40 +1,47 @@
 import React from 'react';
-import { View } from 'react-native-web';
+import { StatusBar, View } from 'react-native-web';
 
 export const foods = [
     {
         id:1,
         title: 'Pääruoat',
+        body: 'Pääruoka vaihtoehtoja',
         img: '../img/carousel_maindish.png'
     },
     {
         id:2,
         title: 'Jälkiruoat',
+        body: 'Jälkiruoka vaihtoehtoja',
         img: '../img/carousel_desserts.png'
     },
     {
         id:3,
         title: 'Kasvisruoat',
+        body: 'Kasvisruoka vaihtoehtoja',
         img: '../img/carousel_maindish.png'
     },
     {
         id:4,
         title: 'Kalaruoat',
+        body: 'Kalaruoka vaihtoehtoja',
         img: '../img/carousel_fish.png'
     },
     {
         id:5,
         title: 'Kanaruoat',
+        body: 'Kanaruoka vaihtoehtoja',
         img: '../img/carousel_maindish.png'
     },
     {
         id:6,
         title: 'Alkuruoat',
+        body: 'Alkuruoka vaihtoehtoja',
         img: '../img/food2.png'
     },
     {
         id:7,
         title: 'Leivät',
+        body: 'Leipä vaihtoehtoja',
         img: '../img/breads.png'
     },
 ]
@@ -47,9 +54,19 @@ export default function FoodPics() {
     }
 }
 
+/* const Item = ({food}) => {
+    return (
+        <View>
+            <Text>{food.body}</Text>
+        </View>
+    )
+} */
+
 return (
-    <View>
-        {testData.map((item, i) =>
-            <Text style={{fontSize:20}} key={i}>{item.title}</Text>)}
+    <View style={{marginTop: StatusBar.currentHeight || 0 }}>
+        <ScrollView>
+            {foodSelection.map((item, i) =>
+                <Text style={{fontSize:20}} key={i}>{item.title}</Text>)}
+        </ScrollView>    
     </View>
 )
--- components/Home.js
+++ components/Home.js
@@ -9,6 +9,7 @@ export default function Home() {
     <ScrollView style={defaultStyle.navMargin}>
         {/* <Text>Home</Text>  */}
         <CarouselCards />
+      
     </ScrollView>
    
 
