acdf94cabbb1488a806c0b13d4d99262e5ef9448
n0jako00 <n0jako00@students.oamk.fi>
2022-12-07 17:37:06 +0200

    Käyttöehdot ja tuki sisältöä

--- components/Terms.js
+++ components/Terms.js
@@ -10,7 +10,10 @@ export default function Terms({ name }) {
 
     return(
     <ScrollView style={defaultStyle.navMargin}>
-        <Text style={defaultStyle.pageTitle}>Käyttöehdot</Text>
+        <Text style={defaultStyle.infoText}>Yleiset käyttöehdot</Text>
+        <Text style={defaultStyle.pageTitle}>Tämä sovellus on Oulun Ammattikorkeakoulun ryhmän TIK21KM projekti, kurssille
+        "Mobile Project IT00CT43-3003". Käyttämällä tätä sovellusta hyväksyt tietosuojaselosteen ja yleiset käyttöehdot.</Text>
+
         <TouchableOpacity onPress={() => navigation.navigate(name)}>
                 <AntDesign name="back" size={18} color="black" />
         </TouchableOpacity>
