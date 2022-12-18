27e96b774410e0d529501d0a266467f3b7672a7b
Valtpit <77121949+Valtpit@users.noreply.github.com>
2022-12-18 14:23:41 +0200

    log

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ import Drinks from './components/Drinks';
 import { useFonts } from 'expo-font';
 
 //poistaa ilmoituksen "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage"
-LogBox.ignoreLogs(["AsyncStorage has been extracted"])
+LogBox.ignoreLogs(["AsyncStorage has been extracted", "Possible", "Each child"])
 
 export default function App() {
 
